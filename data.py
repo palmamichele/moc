f845010cf0e60941214af3d8483914bdc83d3e81
palmamichele <michele.palma@usi.ch>
2026-02-25 15:46:29 +0100

    test: ddataapprox functions

--- data.py
+++ data.py
@@ -33,48 +33,30 @@ def plot_mocs(fmocs, lshmocs, deltas, savename):
 
 
 
-for d in range(2,1000, 100):
 
-    for n in range(2,1000, 100):
-
-            
-        f1 = np.random.rand(2*d, n).astype(np.float64) 
-        #f2 = np.array([3,2,3,3,4,3], dtype=np.float64).reshape(1, -1)#must have shape (d,n), so use this on 1D array
-        x =  np.random.rand(d, n).astype(np.float64) 
+f1 = np.array([1,3,2,5,4,6], dtype=np.float64).reshape(1, -1)
+f2 = np.array([3,2,3,3,4,3], dtype=np.float64).reshape(1, -1)#must have shape (d,n), so use this on 1D array
+x =  np.array([1,2,3,4,5,6], dtype=np.float64).reshape(1, -1)
 
 
-        delta_step = 0.5
-        edmoc = exactdmoc.ExactDiscreteModulusOfContinuity()
-        edmoc.init(x,f1)
+delta_step = 1
+edmoc = exactdmoc.ExactDiscreteModulusOfContinuity()
+edmoc.init(x,f1)
 
-        m1 = edmoc.computeMocPlot(x, f1, delta_step)
-        #m2 = edmoc.computeMocPlot(x, f2, delta_step)
+m1 = edmoc.computeMocPlot(x, f1, delta_step)
 
-        #reconstruct values of t_values, having the size of mocplot
-        t_values = np.zeros(len(m1),dtype=np.float64)
+edmoc.init(x,f2)
+m2 = edmoc.computeMocPlot(x, f2, delta_step)
 
-        for i in range(1, len(t_values)):
-            t_values[i]=t_values[i-1]+delta_step
+print(m2[-1], "same as ", edmoc.computeMoc(x, f2, 7))
 
-        save_name = "data/"+str(d)+"_"+str(n)+"gt_moc"
-        #np.save(save_name+'.npy', m1)
-        #np.save(save_name+'t.npy', t_values)
+#reconstruct values of t_values, having the size of mocplot
+t_values = np.zeros(len(m1),dtype=np.float64)
 
-        #load it back via:  np.load('array.npy')
-        gt_m = np.load(save_name+'.npy')
-        gt_t = np.load(save_name+'t.npy')
-        print("ok")
-        if (not(np.array_equal(gt_m, m1) and np.array_equal(gt_t, t_values))):
-            print("problem!!!" + save_name)
-
-            print(gt_m, "\n")
-
-            print(m1, "\n")
+for i in range(1, len(t_values)):
+    t_values[i]=t_values[i-1]+delta_step
 
 
-        #np.array_equal(arr1, arr2)
-        #np.allclose(a, b)
-
-        #plot_mocs([m1,m1], [], t_values, )
+plot_mocs([m1,m2], [], t_values, "ref_moc.png")
 
 print("end")
